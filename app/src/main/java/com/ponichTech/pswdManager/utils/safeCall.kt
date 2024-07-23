2c1508cdbd45d9c45cebe955901c69b3bc6d7f8f
Niv Ozer <niv.ozer@yahoo.com>
2024-07-23 20:41:15 +0300

    Strings in safeCall - Login - exists but missing context White text in registerFragment is fixed

--- app/src/main/java/com/ponichTech/pswdManager/utils/safeCall.kt
+++ app/src/main/java/com/ponichTech/pswdManager/utils/safeCall.kt
@@ -1,6 +1,9 @@
 package com.ponichTech.pswdManager.utils
 
 import com.google.firebase.auth.FirebaseAuthException
+import com.ponichTech.pswdManager.R
+
+
 
 // function that takes a lambda action, which returns a Resource<T>
 // T is a generic type parameter, allowing this function to work with any type
@@ -18,22 +21,24 @@ inline fun <T> safeCall(action: () -> Resource<T>): Resource<T> {
 }
 fun mapFirebaseErrorCodeToMessage(errorCode: String): String {
     return when (errorCode) {
-        "ERROR_INVALID_CUSTOM_TOKEN" -> "The custom token format is incorrect. Please check the documentation."
-        "ERROR_CUSTOM_TOKEN_MISMATCH" -> "The custom token corresponds to a different audience."
-        "ERROR_INVALID_CREDENTIAL" -> "The supplied credential are malformed."
-        "ERROR_INVALID_EMAIL" -> "email address is badly formatted."
-        "ERROR_WRONG_PASSWORD" -> "The password is invalid or the user does not have a password."
-        "ERROR_USER_MISMATCH" -> "The supplied credentials do not correspond to the previously signed in user."
-        "ERROR_REQUIRES_RECENT_LOGIN" -> "This operation is sensitive and requires recent authentication. Log in again before retrying this request."
-        "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL" -> "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address."
-        "ERROR_EMAIL_ALREADY_IN_USE" -> "The email address is already in use by another account."
-        "ERROR_CREDENTIAL_ALREADY_IN_USE" -> "This credential is already associated with a different user account."
-        "ERROR_USER_DISABLED" -> "The user account has been disabled by an administrator."
-        "ERROR_USER_TOKEN_EXPIRED" -> "The user\\'s credential is no longer valid. The user must sign in again."
-        "ERROR_USER_NOT_FOUND" -> "There is no user record corresponding to this identifier. The user may have been deleted."
-        "ERROR_INVALID_USER_TOKEN" -> "The user\\'s credential is no longer valid. The user must sign in again."
-        "ERROR_OPERATION_NOT_ALLOWED" -> "This operation is not allowed. You must enable this service in the console."
-        "ERROR_WEAK_PASSWORD" -> "The given password is too weak."
+//        "ERROR_INVALID_CUSTOM_TOKEN" -> context.getString(R.string.error_invalid_custom_token)
+//        "ERROR_CUSTOM_TOKEN_MISMATCH" -> context.getString(R.string.error_custom_token_mismatch)
+//        "ERROR_INVALID_CREDENTIAL" -> context.getString(R.string.error_invalid_credential)
+//        "ERROR_INVALID_EMAIL" -> context.getString(R.string.error_invalid_email)
+//        "ERROR_WRONG_PASSWORD" -> context.getString(R.string.error_wrong_password)
+//        "ERROR_USER_MISMATCH" -> context.getString(R.string.error_user_mismatch)
+//        "ERROR_REQUIRES_RECENT_LOGIN" -> context.getString(R.string.error_requires_recent_login)
+//        "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL" -> context.getString(R.string.error_account_exists_with_different_credential)
+//        "ERROR_EMAIL_ALREADY_IN_USE" -> context.getString(R.string.error_email_already_in_use)
+//        "ERROR_CREDENTIAL_ALREADY_IN_USE" -> context.getString(R.string.error_credential_already_in_use)
+//        "ERROR_USER_DISABLED" -> context.getString(R.string.error_user_disabled)
+//        "ERROR_USER_TOKEN_EXPIRED" -> context.getString(R.string.error_user_token_expired)
+//        "ERROR_USER_NOT_FOUND" -> context.getString(R.string.error_user_not_found)
+//        "ERROR_INVALID_USER_TOKEN" -> context.getString(R.string.error_invalid_user_token)
+//        "ERROR_OPERATION_NOT_ALLOWED" -> context.getString(R.string.error_operation_not_allowed)
+//        "ERROR_WEAK_PASSWORD" -> context.getString(R.string.error_weak_password)
+
+
         else -> "An unknown error occurred."
     }
 }
